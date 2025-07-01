2263255efef21da5c9820659b6896a66fd321d55
HQMYL <hqm1256@gmail.com>
2025-07-01 11:51:30 -0600

    Add files via upload

---
+++ eliminar-user.php
@@ -0,0 +1,37 @@
+<?php 
+session_start();
+include("conexion.php");
+
+
+if (isset($_POST['id'])) 
+{
+	$id = $_POST['id'];
+}
+
+#$imagi = array();
+#$imagen = "";
+$sth = $con->prepare("SELECT * FROM users WHERE id_usuario =?");
+$sth->bindParam(1, $id);
+$sth->execute();
+
+if ($sth->rowCount() > 0) {
+
+foreach ($sth as $row ) 
+{
+  $imagen = $row["img"];
+}
+#$imagen = $imagi[0];
+}
+
+unlink("dist/img/users/".$imagen);
+
+
+
+$sql = "DELETE FROM users WHERE id=:id";
+$stmt = $con->prepare($sql);
+$stmt->bindParam(':id', $id, PDO::PARAM_STR); 
+$stmt->execute();
+
+
+
+?>
